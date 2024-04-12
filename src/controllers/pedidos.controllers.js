7b0b26318d0ff5df64bcd849518277f2234c413b
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-04-12 01:27:22 -0300

    agregamos la logica para listar pedidos

--- src/controllers/pedidos.controllers.js
+++ src/controllers/pedidos.controllers.js
@@ -1,5 +1,25 @@
 import Pedido from "../database/model/pedido.js";
 
+export const listarPedidos = async(req, res) => {
+    try {
+      const listaPedidos = await Pedido.find();
+      res.status(200).json(listaPedidos);   
+    } catch (error) {
+      console.log(error)
+      res.status(404).json({mensaje: 'No se pudo encontrar la lista de pedidos'})
+    }
+  };
+
+  export const obtenerPedido = async (req, res)=>{
+    try {
+      const pedidoBuscado = await Pedido.findById(req.params.id);
+      res.status(200).json(pedidoBuscado);
+    } catch (error) {
+      console.log(error)
+      res.status(404).json({ mensaje: 'No se encontro el pedido solicitado'})
+    }
+  }
+
 export const crearPedido = async (req, res) => {
     try {
       const nuevoPedido = new Pedido(req.body);
