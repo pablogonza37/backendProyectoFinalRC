380d276cdccc11de1b087e8b36be1bff4efe0589
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-04-12 20:45:56 -0300

    agregamos las validaciones de usuarios

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -8,10 +8,11 @@ import {
   suspenderUsuario,
   levantarSuspensionUsuario
 } from "../controllers/usuarios.controllers.js";
+import validacionesUsuario from "../helpers/validacionUsuario.js";
 
 const router = Router();
 
-router.route("/usuarios").post(crearUsuario).get(listarUsuarios);
+router.route("/usuarios").post([validacionesUsuario], crearUsuario).get(listarUsuarios);
 router.route("/usuarios/:id").get(obtenerUsuario).delete(borrarUsuario);
 router.route('/suspender/:id').put(suspenderUsuario);
 router.route('/levantar-suspension/:id').put(levantarSuspensionUsuario);
