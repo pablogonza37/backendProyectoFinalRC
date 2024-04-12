e114c154d920750b5b6ec788b7b8877f6f5da01d
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-04-12 00:52:01 -0300

    se configuraron los middlewares

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from "cors";
 import "dotenv/config";
+import morgan from "morgan";
+import {fileURLToPath} from 'url';
+import path from 'path';
 
 const app = express();
 app.set("port", process.env.PORT || 4000);
@@ -8,3 +11,11 @@ app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"), () => {
   console.log("Estoy en el puerto " + app.get("port"));
 });
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname,'/public')));
