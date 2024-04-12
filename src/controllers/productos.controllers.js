c659f9ac740184e007617ce450cd2d1f23277a79
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-04-12 01:07:33 -0300

    se creo la logica para listar productos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,9 +1,24 @@
 import Producto from "../database/model/producto.js";
 
-export const listarProductos = (req, res)=>{
-    console.log('desde listar productos');
-    res.send('enviar lista de productos...')
-}
+export const listarProductos = async(req, res) => {
+    try {
+      const productos = await Producto.find();
+      res.status(200).json(productos);   
+    } catch (error) {
+      console.log(error)
+      res.status(404).json({mensaje: 'No se pudo encontrar la lista de productos'})
+    }
+  };
+
+  export const obtenerProducto = async (req, res)=>{
+    try {
+      const productoBuscado = await Producto.findById(req.params.id);
+      res.status(200).json(productoBuscado);
+    } catch (error) {
+      console.log(error)
+      res.status(404).json({ mensaje: 'No se encontro el producto solicitado'})
+    }
+  }
 
 export const crearProducto = async (req, res) => {
     try {
