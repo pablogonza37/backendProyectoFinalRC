84279335fe80f163b7a49d8afbf93e7d61cfaadb
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-04-12 01:30:33 -0300

    agregamos la logica necesaria para eliminar pedidos

--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { crearPedido, listarPedidos, obtenerPedido } from "../controllers/pedidos.controllers.js";
-
+import {
+  borrarPedido,
+  crearPedido,
+  listarPedidos,
+  obtenerPedido,
+} from "../controllers/pedidos.controllers.js";
 
 const router = Router();
 
-router.route('/pedidos').post(crearPedido).get(listarPedidos);
-router.route('/pedidos/:id').get(obtenerPedido);
+router.route("/pedidos").post(crearPedido).get(listarPedidos);
+router.route("/pedidos/:id").get(obtenerPedido).delete(borrarPedido);
 
 export default router;
