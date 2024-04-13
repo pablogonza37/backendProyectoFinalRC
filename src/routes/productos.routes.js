376c5af2272af957a20422e197c4a2c76fd3c435
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-04-13 15:46:40 -0300

    se agrego validacion con jwt para crear productos

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -7,14 +7,15 @@ import {
   editarProducto,
 } from "../controllers/productos.controllers.js";
 import validacionesProducto from "../helpers/validacionProducto.js";
+import validarJWT from "../helpers/verificarJWT.js";
 
 const router = Router();
 
-router.route("/productos").get(listarProductos).post([validacionesProducto], crearProducto);
+router.route("/productos").get(listarProductos).post([validarJWT,validacionesProducto], crearProducto);
 router
   .route("/productos/:id")
   .get(obtenerProducto)
-  .delete(borrarProducto)
-  .put(editarProducto);
+  .delete(validarJWT, borrarProducto)
+  .put([validarJWT,validacionesProducto],editarProducto);
 
 export default router;
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -9,11 +9,12 @@ import {
   levantarSuspensionUsuario
 } from "../controllers/usuarios.controllers.js";
 import validacionesUsuario from "../helpers/validacionUsuario.js";
+import validarJWT from "../helpers/verificarJWT.js";
 
 const router = Router();
 
 router.route("/usuarios").post([validacionesUsuario], crearUsuario).get(listarUsuarios);
-router.route("/usuarios/:id").get(obtenerUsuario).delete(borrarUsuario);
+router.route("/usuarios/:id").get(obtenerUsuario).delete(validarJWT, borrarUsuario);
 router.route('/suspender/:id').put(suspenderUsuario);
 router.route('/levantar-suspension/:id').put(levantarSuspensionUsuario);
 router.route("/login").post(login);
