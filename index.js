1cb1d38d4c88d7abe7627e41691ed00f7b0a5e4a
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-04-12 21:15:35 -0300

    se quitaron los console.log

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ import usuarioRouter from './src/routes/usuarios.routes.js';
 const app = express();
 app.set("port", process.env.PORT || 4000);
 
-app.listen(app.get("port"), () => {
-  console.log("Estoy en el puerto " + app.get("port"));
-});
+app.listen(app.get("port"));
 
 app.use(cors());
 app.use(morgan("dev"));
