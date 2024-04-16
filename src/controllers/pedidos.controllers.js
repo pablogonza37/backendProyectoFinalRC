3770e8342440efcdfbec8bca72cc72f126b8b9ef
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-04-15 23:34:16 -0300

    se agrego la funcion de agregar cantidad del pedido

--- src/controllers/pedidos.controllers.js
+++ src/controllers/pedidos.controllers.js
@@ -79,3 +79,25 @@ export const crearPedido = async (req, res) => {
       });
     }
   };
+
+  export const cambiarCantidadPedido = async (req, res) => {
+    try {
+      const buscarPedido = await Pedido.findById(req.params.id);
+      if (!buscarPedido) {
+        return res
+          .status(404)
+          .json({
+            mensaje: "No se pudo editar el producto, el id es incorrecto.",
+          });
+      }
+      await Pedido.findByIdAndUpdate(req.params.id, req.body);
+      res
+        .status(200)
+        .json({ mensaje: "El producto fue modificado exitosamente" });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ mensaje: "Ocurrio un error al intentar editar el producto" });
+    }
+  };
--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -4,7 +4,8 @@ import {
   crearPedido,
   listarPedidos,
   obtenerPedido,
-  cambiarEstadoPedido
+  cambiarEstadoPedido,
+  cambiarCantidadPedido
 } from "../controllers/pedidos.controllers.js";
 import validacionesPedido from "../helpers/validacionPedido.js";
 
@@ -12,5 +13,6 @@ const router = Router();
 
 router.route("/pedidos").post([validacionesPedido], crearPedido).get(listarPedidos);
 router.route("/pedidos/:id").get(obtenerPedido).delete(borrarPedido).put(cambiarEstadoPedido);
+router.route("/cantidadPedido/:id").put(cambiarCantidadPedido)
 
 export default router;
