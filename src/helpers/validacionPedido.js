644abd69dc981be8f8c5a33e999ae93d11b73a41
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-04-17 16:01:06 -0300

    se agrego direccion al pedido

--- src/helpers/validacionPedido.js
+++ src/helpers/validacionPedido.js
@@ -40,6 +40,23 @@ const validacionesPedido = [
     check("cantidad")
     .notEmpty()
     .withMessage("El usuario es un dato obligatorio"),
+    check("precioTotal")
+    .notEmpty()
+    .withMessage("El precio toltal es un dato obligatorio")
+    .isNumeric()
+    .withMessage("El precio total debe ser un número")
+    .custom((value) => {
+      if (value >= 50 && value <= 2000000) {
+        return true;
+      } else {
+        throw new Error("El precio total debe estar entre $50 y $2000000");
+      }
+    }),
+    check("direccion")
+    .notEmpty()
+    .withMessage("La direccion es un dato obligatorio")
+    .isLength({ min: 2, max: 200 })
+    .withMessage("La direccion debe tener entre 2 y 200 caracteres"),
   (req, res, next) => {
     resultadoValidacion(req, res, next);
   },
